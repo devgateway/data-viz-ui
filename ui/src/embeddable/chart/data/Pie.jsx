6c4720e9fb121b7c8241db55bd6e7c0356ab487c
Sebastian Dimunzio <sdimunzio@gmail.com>
2023-04-11 16:37:05 -0300

    adding front files

---
+++ ui/src/embeddable/chart/data/Pie.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+
+import {measuresMap, typesMap, getTranslatedValue} from './Utils'
+
+export const PieData = (props) => {
+    const {children, data, measures, locale} = props
+    const dimensionsMetadata = new Set()
+    const measuresMetadata = new Set()
+    const mMap = measuresMap(data)
+
+    const varValues = {}
+    
+    if (data && data.children) {
+        const values = []
+        const tMap = typesMap(data)
+        const keys = []
+        data.children.forEach(d => {
+            if (d.children) {
+                d.children.forEach(d1 => {
+                    const row = new Object()
+                    const variables = new Object()
+                    variables[d.type] = getTranslatedValue(tMap[d.type].items.filter(i => i.value === d.value)[0], locale) || d.value
+                    variables[d1.type] = getTranslatedValue(tMap[d1.type].items.filter(i => i.value === d1.value)[0], locale) || d1.value
+
+                    Object.keys(d1).forEach(k => {
+                        variables[k] = d1[k]
+                    })
+                    row.id = getTranslatedValue(tMap[d.type].items.filter(i => i.value === d.value)[0], locale) + ' - ' + getTranslatedValue(tMap[d1.type].items.filter(i => i.value === d1.value)[0], locale)  //Male /African ect (dimension value)
+                    keys.push(d.value + ' - ' + d1.value)
+                    row.value = d1[measures[0]]
+                    row.label = getTranslatedValue(tMap[d.type].items.filter(i => i.value === d.value)[0], locale) + ' - ' + getTranslatedValue(tMap[d1.type].items.filter(i => i.value === d1.value)[0], locale)  //Male /African ect (dimension value)
+                    row.variables = variables
+                    values.push(row)
+                })
+            } else {
+                const category = tMap[d.type].items.filter(i => i.value === d.value)[0]                
+                const row = new Object()
+                const variables = new Object()
+                variables[d.type] = getTranslatedValue(category, locale) || d.value
+                Object.keys(data).forEach(k => {
+                    variables[k] = d[k]
+                })
+
+                dimensionsMetadata.add(tMap[d.type])
+                row.id = getTranslatedValue(category, locale) || d.value
+                row.label = getTranslatedValue(category, locale) || d.value
+                row.position = category.position || 0
+                row.value = d[measures[0]]
+                row.variables = variables
+                values.push(row)
+            }
+
+        })
+
+        const options = {
+            indexBy: 'id',
+            keys: [],
+            measuresMetadata,
+            dimensionsMetadata,
+            data: values.sort((d1, d2) => d2.value - d1.value),
+            metadata: data.metadata
+        }
+        return React.Children.map(children, child => React.cloneElement(child, {options}))
+    } else {
+
+
+        const selectedMeasures = data.metadata.measures.filter(m => measures.includes(m.value))
+        const values = []
+
+        const variables = {}
+        Object.keys(data).forEach(k => {
+            variables[k] = data[k]
+        })
+        
+
+        selectedMeasures.forEach(m => {            
+            let row = {}            
+            row.type = "measure"
+            row["measureFieldName"] = m.value
+            row["id"] = getTranslatedValue(mMap[m.value], locale)
+            row["position"] = m.position
+            row["label"] = getTranslatedValue(mMap[m.value], locale)
+            row["value"] = data[m.value]
+            row.variables = variables
+            values.push(row)
+        })
+ 
+        const options = {
+            indexBy: 'id',
+            keys: [],
+            measuresMetadata,
+            dimensionsMetadata,
+            data: values.sort((d1, d2) => d2.value - d1.value),
+            metadata: data.metadata
+        }
+
+
+        //No dimensions selected
+        return React.Children.map(children, child => React.cloneElement(child, {options}))
+    }
+}
+
+export default PieData
---
+++ ui/src/layout/SearchControl.jsx
@@ -0,0 +1,124 @@
+import React, {useEffect, useState} from "react";
+import CustomSearch from './CustomSemanticSearch'
+import SearchProvider from "@devgateway/wp-react-lib/dist/providers/SearchProvider";
+import SearchConsumer from "@devgateway/wp-react-lib/dist/consumers/SearchConsumer";
+import {injectIntl} from "react-intl";
+import {utils} from "@devgateway/wp-react-lib";
+import CustomSemanticSearch from "./CustomSemanticSearch";
+
+
+const resultRenderer = injectIntl(({
+                                       ID,
+                                       title,
+                                       slug,
+                                       parent_title,
+                                       parent_slug,
+                                       parent_link,
+                                       extract,
+                                       type,
+                                       link,
+                                       terms,
+                                       subtype,
+                                       bread_crumbs = [],
+                                       metadata: {redirect_url},
+                                       intl: {locale}
+                                   }) => {
+
+
+    let target = parent_link ? utils.replaceLink(parent_link, locale) + `#${slug}` : utils.replaceLink(link, locale)
+    target = redirect_url ? redirect_url + `#${slug}` : target
+
+    
+
+    return (
+        <div className={"has-standard-12-font-size"} onClick={e => document.location.href = target}>
+            <h5>{bread_crumbs && bread_crumbs.length > 0 ? `${bread_crumbs.join(' / ')}` : ''} </h5>
+            <div className={"has-standard-14-font-size"}><h4>{title}</h4></div>
+            <div className='search-content' dangerouslySetInnerHTML={{__html: utils.replaceHTMLinks(extract, locale)}}/>
+            <br/>
+        </div>
+    )
+})
+
+const replaceString = (content, words) => {
+    let regex = RegExp(words, 'gi')  
+    let newHTML = content
+    let instances = [...(newHTML.matchAll(regex))]
+    let shift = 0
+    let lengthBeforeChange = newHTML.length
+    instances.forEach(instance => {                             
+        let replacement = '<b>' + newHTML.substring(instance.index + shift, instance.index + shift + words.length) + '</b>';
+        newHTML = newHTML.substring(0, instance.index + shift) + replacement + newHTML.substring(instance.index + words.length + shift);
+        shift = newHTML.length - lengthBeforeChange;              
+    }) 
+
+    return newHTML;
+}
+
+const searchTextHandler = (words) => {
+    let searchedPara = document.querySelector('.results');
+    let searchResultHeading = searchedPara = searchedPara = document.querySelectorAll('H5')
+    let searchResult = searchedPara = searchedPara = document.querySelectorAll('.search-content')
+    for (let i = 0; i < searchResult.length; i++) {
+        if (searchResult[i]) {          
+            searchResult[i].innerHTML = replaceString(searchResult[i].textContent, words);
+        }      
+    }
+    
+    for (let i = 0; i < searchResultHeading.length; i++) {
+       if (searchResultHeading[i]) {
+            searchResultHeading[i].innerHTML = replaceString(searchResultHeading[i].textContent, words);
+      }        
+    }
+}
+
+
+const SearchControl = ({onSearch,perPage, loading, results, meta}) => {
+    const total = meta ? meta['x-wp-total'] : 0
+    const totalPages = meta ? meta['x-wp-totalpages'] : 0
+
+
+    const [searchTerm, setSearchTerm] = useState('')
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            onSearch(searchTerm)
+        }, 300)
+
+        return () => clearTimeout(delayDebounceFn)
+    }, [searchTerm])
+
+
+    useEffect(() => searchTextHandler(searchTerm), [results])
+
+    return (<CustomSemanticSearch
+            value={searchTerm}
+            loading={loading}
+            placeholder='Search...'
+            onResultSelect={(e, data) => null}
+            total={total}
+            perPage={perPage}
+            totalPages={totalPages}
+            onSearchChange={(a, b) => {
+                setSearchTerm(b.value)
+            }}
+            searchTextHandler={searchTextHandler}
+            resultRenderer={resultRenderer}
+            results={results}
+            showNoResults={false}
+
+        />
+    )
+}
+
+const SearchComponent = injectIntl((props) => {
+    const {intl} = props
+    const [query, setQuery] = useState("")
+    return (<SearchProvider search={query} perPage={5} locale={intl.locale}>
+        <SearchConsumer>
+            <SearchControl  onSearch={setQuery}  perPage={5}></SearchControl>
+        </SearchConsumer>
+    </SearchProvider>)
+})
+
+
+export default SearchComponent
