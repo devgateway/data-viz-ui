816cf715803520c9a2ba20613a8da9dc77740965
Sebastian Dimunzio <sdimunzio@gmail.com>
2023-06-01 08:28:20 -0300

    adding layers,labels, colors and zoom on d3map

--- ui/src/embeddable/d3Map/index.jsx
+++ ui/src/embeddable/d3Map/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {parse} from "../utils/parseUtils";
+import {decode, parse} from "../utils/parseUtils";
 import Map from "./Map"
 import BaseLayer from './BaseLayer'
 import DataLayer from './DataLayer'
 import ZoomControl from "./ZoomControl";
 import {Container} from "semantic-ui-react";
+import ProjectedContainer from "./ProjectedContainer";
+
 const MapWrapper = (props) => {
     const {
         unique,
@@ -13,25 +15,32 @@ const MapWrapper = (props) => {
         "data-layers": dataLayers,
         "data-height": height = 600,
         "data-back-ground-color": bgColorParam = '#88e8dc',
+        "data-map-position": paramMapPosition = {}
     } = props
 
     const layers = parse(dataLayers)
     const [transform, setZoomTransform] = React.useState(null)
+
+    
+
     return (
-        <Container fluid className={"d3Map"} style={{backgroundColor:decodeURIComponent(bgColorParam)}}>
-            <Map height={height}>
-                {layers.map((layer, i) => {
-                    if (layer.type === 'base') {
-                        return <BaseLayer transform={transform} key={i} {...layer} />
-                    }
-                    if (layer.type === 'data') {
-                        return <DataLayer transform={transform} key={i} {...layer} />
-                    }
+        <Container  className={"d3map-container"}
+                   style={{backgroundColor: decode(bgColorParam), height: height+"px"}}>
+            <ProjectedContainer editing={editing}  initialPosition={parse(paramMapPosition, editing)}>
+                <Map>
+                    {layers.map((layer, i) => {
+                        if (layer.type === 'base') {
+                            return <BaseLayer key={i} {...layer} />
+                        }
+                        if (layer.type === 'data') {
+                            return <DataLayer key={i} {...layer} />
+                        }
 
-                })}
+                    })}
+                </Map>
+                <ZoomControl editing={editing}/>
 
-            </Map>
-            <ZoomControl onZoomEnd={setZoomTransform}/>
+            </ProjectedContainer>
         </Container>
     );
 
