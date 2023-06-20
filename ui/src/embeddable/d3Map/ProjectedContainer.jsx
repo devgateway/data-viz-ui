757b5fc995eb1a5f696d8de6ab6c3346bfe6e9ec
Sebastian Dimunzio <sdimunzio@gmail.com>
2023-06-19 23:08:43 -0300

    partially done d3 map

--- ui/src/embeddable/d3Map/ProjectedContainer.jsx
+++ ui/src/embeddable/d3Map/ProjectedContainer.jsx
@@ -1,4 +1,4 @@
-import React, {createRef, useEffect, useState} from 'react';
+import React, {Children, createRef, useEffect, useState} from 'react';
 import {connect} from "react-redux";
 import * as topojson from "topojson-client";
 import * as d3 from 'd3' // d3 plugin
@@ -7,30 +7,44 @@ import * as d3 from 'd3' // d3 plugin
 class ProjectedContainer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {path: null, projection: null}
+        this.state = {path: null, projection: null, width: 0, height: 0}
         this.divRef = React.createRef();
+        this.getHeight = this.getHeight.bind(this)
+        this.getWidth = this.getWidth.bind(this)
+        this.project=this.project.bind(this)
     }
 
     getHeight() {
-        return this.divRef.current ? this.divRef.current.parentNode.offsetHeight : 0;
-
+        return this.divRef.current.parentNode.offsetHeight ;
     }
 
     getWidth() {
-        return this.divRef.current ? this.divRef.current.parentNode.offsetWidth : 0;
+        return this.divRef.current.parentNode.offsetWidth ;
     }
 
-    componentDidMount() {
-        const {scale = 190, center = [0, 0], initialPosition: {x = 0, y = 0, k = 0}} = this.props
+    compo
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+
+
+    }
+
+
+    project() {
+        const {scale = 190, center = [0, 0], initialPosition: {x = 0, y = 0, k = 0}} = this.props
         const projection = d3.geoMercator()
             .scale(scale)
             .center(center)  // centers map at given coordinates
             .translate([this.getWidth() / 2, this.getHeight() / 2])
-
-
         const path = d3.geoPath().projection(projection);
-        this.setState({path, projection})
+        window.setTimeout(()=>this.setState({path, projection,height:this.getHeight(),width:this.getWidth()}),500)
+
+    }
+
+    componentDidMount() {
+        this.project()
+       window.addEventListener("resize", this.project);
+
 
     }
 
@@ -39,20 +53,23 @@ class ProjectedContainer extends React.Component {
     }
 
     render() {
-        const {scale = 190, center = [0, 0], initialPosition} = this.props
+        const {initialPosition} = this.props
 
-        return <div ref={this.divRef}
-                    className={"d3Map"}>
-            {this.state.path ? this.props.children.map(child => {
+        const arrayChildren = Children.toArray(this.props.children);
+
+
+        return <div ref={this.divRef} className={"d3Map"}>
+
+            {this.divRef.current&&Children.map(arrayChildren, child => {
                 return React.cloneElement(child, {
                     ...this.state,
                     initialPosition,
-                    height: this.getHeight(),
-                    width: this.getWidth(),
                     editing: this.props.editing
 
-                },)
-            }) : null}
+                })
+
+            })}
+
         </div>
     }
 }
