82e8fdcfb0e760e4adf18c1a38e8a2bdfb96d51a
Sebastian Dimunzio <sdimunzio@gmail.com>
2023-06-22 09:37:26 -0300

    fixing zoom and adding point labels

--- ui/src/embeddable/d3Map/index.jsx
+++ ui/src/embeddable/d3Map/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useLayoutEffect, useRef, useState} from 'react';
 import {connect} from "react-redux";
 import {decode, parse} from "../utils/parseUtils";
 import Map from "./Map"
@@ -9,58 +9,62 @@ import {Container} from "semantic-ui-react";
 import ProjectedContainer from "./ProjectedContainer";
 
 const MapWrapper = (props) => {
-    const {
-        unique,
-        editing,
-        "data-layers": dataLayers,
-        "data-height": height = 600,
-        "data-back-ground-color": bgColorParam = '#88e8dc',
-        "data-map-position": paramMapPosition = {}
-    } = props
+        const {
+            unique,
+            editing,
+            "data-group": group,
+            "data-layers": dataLayers,
+            "data-height": height = 400,
+            "data-width": width = 1000,
+            "data-back-ground-color": bgColorParam = '#88e8dc',
+            "data-map-position": paramMapPosition = {}
+        } = props
 
-    const layers = parse(dataLayers)
-    const [transform, setZoomTransform] = React.useState(null)
-    const [readyState, setReadyState] = React.useState(false)
-    const layerCreated = []
-    const onLayerCreated = (layer) => {
-        layerCreated.push(layer)
-        
-        if (layerCreated.length == layers.length) {
-            setReadyState(true)
+        const layers = parse(dataLayers)
+        const [readyState, setReadyState] = React.useState(false)
+        const layerCreated = []
+
+        const ref = useRef(null);
+        const onLayerCreated = (layer) => {
+            layerCreated.push(layer)
+            if (layerCreated.length == layers.length) {
+                setReadyState(true)
+            }
         }
 
+
+        return (
+            <div ref={ref} className={"d3map-container"}>
+
+                <ProjectedContainer backgroundColor={decode(bgColorParam)}
+                                    height={height}
+                                    width={width}
+                                    editing={editing} initialPosition={parse(paramMapPosition, editing)}>
+                    <Map>
+                        {layers.map((layer, i) => {
+                            if (layer.type === 'base') {
+                                return <BaseLayer unique={unique} onLayerCreated={e => onLayerCreated(layer)}
+                                                  key={i} {...layer} />
+                            }
+                            if (layer.type === 'data') {
+                                return <DataLayer unique={unique} onLayerCreated={e => onLayerCreated(layer)}
+                                                  key={i} {...layer} />
+                            }
+
+                        })}
+                    </Map>
+                    <ZoomControl group={group} readyState={readyState} editing={editing}/>
+                </ProjectedContainer>
+            </div>
+        );
+
     }
+;
 
-
-    
-    return (
-        <Container className={"d3map-container"}
-                   style={{backgroundColor: decode(bgColorParam), height: height + "px", width:"1190px"}}>
-            <ProjectedContainer height={height} editing={editing} initialPosition={parse(paramMapPosition, editing)}>
-                <Map>
-                    {layers.map((layer, i) => {
-                        if (layer.type === 'base') {
-                            return <BaseLayer unique={unique} onLayerCreated={e => onLayerCreated(layer)} key={i} {...layer} />
-                        }
-                        if (layer.type === 'data') {
-                            return <DataLayer unique={unique} onLayerCreated={e => onLayerCreated(layer)}
-                                              key={i} {...layer} />
-                        }
-
-                    })}
-                </Map>
-                <ZoomControl readyState={readyState} editing={editing}/>
-            </ProjectedContainer>
-        </Container>
-    );
-
+const mapStateToProps = (state, ownProps) => {
+    return {}
 }
-    ;
 
-    const mapStateToProps = (state, ownProps) => {
-        return {}
-    }
+const mapActionCreators = {};
 
-    const mapActionCreators = {};
-
-    export default connect(mapStateToProps, mapActionCreators)(MapWrapper)
+export default connect(mapStateToProps, mapActionCreators)(MapWrapper)
