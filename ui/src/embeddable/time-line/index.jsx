de50a508e366f4974ee8b6f4133b97d1a2176f7f
allan_dg <amogusu@developmentgateway.org>
2024-09-09 14:15:55 +0300

    Implement mobile responsive timeline component

--- ui/src/embeddable/time-line/index.jsx
+++ ui/src/embeddable/time-line/index.jsx
@@ -4,6 +4,8 @@ import "pure-react-carousel/dist/react-carousel.es.css";
 import React, { useEffect, useRef, useState } from "react";
 import { Container } from "semantic-ui-react";
 import * as d3 from "d3";
+import Carousel from './mobile';
+import getDeviceType from "../../utils/deviceType";
 
 const visibleStyle = {
   visibility: "visible",
@@ -19,30 +21,31 @@ const hiddenStyle = {
 };
 
 const DEFAULT_HIGHLIGHTED_POST = 0;
-const TimeLine = ({
-  posts,
-  position,
-  lineWidth,
-  meta,
-  locale,
-  lineColor,
-  height,
-  config,
-  marginLeft,
-  marginTop,
-  marginRight,
-  marginBottom,
-  fontSize,
-  titleWidth,
-  titleHeight,
-  subtitleWidth,
-  subtitleHeight,
-  enableTitlePopup,
-  enableCirclePopup,
-  enableDefaultPopup,
-  closePopupOnMouseOut,
-  unique,
-}) => {
+const TimeLine = (props) => {
+  let {
+    posts,
+    position,
+    lineWidth,
+    meta,
+    locale,
+    lineColor,
+    height,
+    config,
+    marginLeft,
+    marginTop,
+    marginRight,
+    marginBottom,
+    fontSize,
+    titleWidth,
+    titleHeight,
+    subtitleWidth,
+    subtitleHeight,
+    enableTitlePopup,
+    enableCirclePopup,
+    enableDefaultPopup,
+    closePopupOnMouseOut,
+    unique,
+  } = props;
   const ref = useRef();
   const [displayTooltip, setDisplayTooltip] = useState(false);
   const [tooltipData, setTooltipData] = useState(null);
@@ -489,4 +492,14 @@ const PostCarousel = (props) => {
     </Container>
   );
 };
-export default PostCarousel;
+
+let carousel;
+if(['mobile', 'tablet', 'midTablet'].includes(getDeviceType())) {
+  console.log('here...')
+    carousel = Carousel
+} else {
+    carousel = PostCarousel
+}
+
+export default carousel;
+
