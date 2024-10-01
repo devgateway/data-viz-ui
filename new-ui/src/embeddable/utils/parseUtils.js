5c7a0863bfbaa0b495d0667a9d48dec94710cc3e
Timothy Mugo <tmugo@developmentgateway.org>
2024-10-01 15:16:50 +0300

    DVIZ-10 fix react router and configure wordpress

---
+++ new-ui/src/embeddable/utils/parseUtils.js
@@ -0,0 +1,21 @@
+export const decode = (value, editing) => {
+    if (editing) {
+        return value
+    }
+    return decodeURIComponent(value.replace(/%(?![0-9a-fA-F][0-9a-fA-F]+)/g, '%25')) //replaces "%" character before decode URI to avoid errors with URI codes
+}
+
+export const parse = (value, editing) => {
+    try {
+        return JSON.parse(decode(value, editing))
+    } catch (error) {
+        console.error("error parsing value:" + value)
+    }
+    return null
+}
+
+export const compareJsonProps = (p1, p2) => {
+    return JSON.stringify(p1) === JSON.stringify(p2)
+}
+
+export default {decode, parse, compareJsonProps}
--- new-ui/src/redux/reducer.ts
+++ new-ui/src/redux/reducer.ts
@@ -1,14 +1,12 @@
 import {combineReducers} from 'redux-immutable';
-// import {connectRouter} from 'connected-react-router/immutable'
-// import {intlReducer} from 'react-intl-redux'
 import {wordpress} from "@devgateway/wp-react-lib"
-// import {reducers} from '../embeddable/'
+import {reducers} from '../embeddable'
 import { createReduxHistoryContext } from "redux-first-history";
 import { createBrowserHistory } from "history";
+import { intlReducer } from '@/lib/react-intl-redux';
 
 const {
     createReduxHistory,
-    createReduxHistoryEnhancer,
     routerMiddleware,
     routerReducer
 } =
@@ -19,8 +17,9 @@ const {
 const createRootReducer = () => combineReducers({
     router: routerReducer,
     wordpress,
-    // intl: intlReducer
+    ...reducers,
+    intl: intlReducer
 });
 
-export { createReduxHistory, createReduxHistoryEnhancer, routerMiddleware };
+export { createReduxHistory, routerMiddleware };
 export default createRootReducer
