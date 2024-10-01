ebec1956d45b790f1b4bb74e230dc96830077656
Timothy Mugo <tmugo@developmentgateway.org>
2024-10-01 20:33:35 +0300

    DVIZ-10 add missing routes

---
+++ new-ui/src/layout/containers/SlugContainer.tsx
@@ -0,0 +1,27 @@
+import PageConsumer from '@devgateway/wp-react-lib/dist/consumers/PageConsumer';
+import PageProvider from '@devgateway/wp-react-lib/dist/providers/PageProvider';
+import React from 'react'
+import { useParams } from 'react-router-dom';
+import ResponsiveContainer from '../ResponsiveContainer';
+import Page from '@devgateway/wp-react-lib/dist/templates/Page';
+import Helmet from '@/Helmet';
+
+const SlugContainer = () => {
+    const { locale, slug } = useParams();
+    return (
+        <PageProvider
+            locale={locale}
+            slug={slug}
+            store={slug}>
+            <ResponsiveContainer>
+                <PageConsumer>
+
+                    <Helmet locale={locale} pages={<Page/>}></Helmet>
+                    <Page></Page>
+                </PageConsumer>
+            </ResponsiveContainer>
+        </PageProvider>
+    )
+}
+
+export default SlugContainer
