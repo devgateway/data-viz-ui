f77a972aad95100aa512afd807a770f7bc98d41d
Timothy Mugo <tmugo@developmentgateway.org>
2024-10-01 15:16:50 +0300

    DVIZ-10 fix react router and configure wordpress

--- new-ui/src/withTracker.jsx
+++ new-ui/src/withTracker.jsx
@@ -1,38 +1,29 @@
-import React, {Component} from 'react';
+import React, { useEffect } from 'react';
 
 import ReactGA from "react-ga4";
-
+import { useLocation } from 'react-router-dom';
 
 
 
 ReactGA.initialize('#REACT_APP_GA_CODE#');
 
+
 const withTracker = (WrappedComponent, options = {}) => {
+    const HOC = (props) => {
+        const location = useLocation();
 
+        useEffect(() => {
+            const page = location.pathname;
+            ReactGA.send({ hitType: "pageview", page });
+        }, [location.pathname]);
 
-    const HOC = class extends Component {
-        componentDidMount() {
+        React.useEffect(() => {
+            return () => {
+                // Cleanup if needed
+            };
+        }, []);
 
-            const page = this.props.location.pathname;
-            ReactGA.send({hitType: "pageview", page});
-
-            //trackPage(page);
-        }
-
-        componentDidUpdate = prevPros => {
-            const currentPage = prevPros.location.pathname;
-            const nextPage = this.props.location.pathname;
-
-            if (currentPage !== nextPage) {
-                ReactGA.send({hitType: "pageview", page: nextPage});
-
-            }
-        };
-
-        render() {
-
-            return <WrappedComponent {...this.props} />;
-        }
+        return <WrappedComponent {...props} />;
     };
 
     return HOC;
