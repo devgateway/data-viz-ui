129e4d783a1c28308759c3dcb9accd8bed770ebd
Timothy Mugo <tmugo@developmentgateway.org>
2024-10-01 13:40:57 +0300

    DVIZ-10 add app configuration, add eslint configuration

---
+++ new-ui/src/Helmet.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import ReactHtmlParser from 'html-react-parser';
+
+console.log("env==>",process.env)
+
+const useHash = process.env.REACT_APP_USE_HASH_LINKS === true;
+
+export const replaceHTMLinks = (html, locale) => {
+
+    const replacementTarget = process.env.REACT_APP_WP_HOSTS.split(",");
+
+    let all = new RegExp("^(http|https)://(" + replacementTarget.join('|') + ")", "ig");
+    let link;
+    let regex = /(['"])(https?:\/\/.+?)\1/ig;
+    let newHtml = html;
+
+    while ((link = regex.exec(html)) !== null) {
+
+        let href = link[2];
+        let newLink;
+
+        if (href.indexOf("wp-content") === -1) {
+            if (useHash) {
+                newLink = href.replace(all, '#' + locale); //TODO:fix it!
+            } else {
+                newLink = href.replace(all, '/' + locale); //TODO:fix it!
+            }
+            newHtml = newHtml.replaceAll(link[2], newLink);
+        } else {
+            console.log(href)
+        }
+    }
+
+    return newHtml;
+};
+
+const helmetContext = {}
+
+
+const HelmetMetadata = ({pages, locale}) => {
+
+    const {yoast_head_json} = pages[0];
+
+    return (
+        <HelmetProvider context={helmetContext}>
+            <Helmet>
+                {ReactHtmlParser(pages[0].yoast_head)}
+            </Helmet>
+        </HelmetProvider>
+
+    );
+
+}
+
+export default HelmetMetadata
---
+++ new-ui/src/layout/ResponsiveContainer.jsx
@@ -0,0 +1,59 @@
+import React, {Component, useEffect, useState} from 'react'
+
+import {Container, Icon, Menu, Sidebar,} from 'semantic-ui-react'
+import PropTypes from 'prop-types'
+import {SettingsConsumer} from '@devgateway/wp-react-lib'
+import {Media} from "../AppMedia"
+import Footer from "./Footer.jsx";
+import Header from "./Header.jsx";
+import TopNavigator from "./TopNavigator.jsx";
+import CustomizerWrapper from "./Customizer.jsx";
+
+
+class DesktopContainer extends Component {
+    render() {
+        const {children, fixed} = this.props
+        return (
+
+            <Container fluid>
+                <SettingsConsumer>
+                    <CustomizerWrapper>
+                        <Header></Header>
+                    </CustomizerWrapper>
+                </SettingsConsumer>
+                <Container className="desktop">
+                    {children}
+                </Container>
+                <TopNavigator/>
+            </Container>
+
+
+        )
+    }
+}
+
+DesktopContainer.propTypes = {
+    children: PropTypes.node,
+}
+
+
+class ResponsiveContainer extends Component {
+
+
+    render() {
+        const {children, fixed, locale, pages} = this.props
+        const page = pages ? pages[0] : null;
+        return (<div>
+            <style>
+                {Media.mediaStyles}
+            </style>
+            <DesktopContainer fixed={fixed}>
+                {children}
+            </DesktopContainer>
+            {page && page.template === "noofoter.php" ? "" : <Footer></Footer>}
+        </div>)
+    }
+}
+
+
+export default ResponsiveContainer
