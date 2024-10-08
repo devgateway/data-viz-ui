80ef76ced8c818849743b3c29cb2a2783d7086aa
Timothy Mugo <tmugo@developmentgateway.org>
2024-10-08 16:46:31 +0300

    DVIZ-10 ui fixes

--- new-ui/src/embeddable/map/types.ts
+++
@@ -1,9 +0,0 @@
-export interface DataFrameProps {
-    children: React.ReactNode,
-    data?: any,
-    measures: string,
-    customMeasureLabels: any,
-    multipleMeasures: boolean,
-    mapType: string,
-    aggregationFormula: string,
-}
