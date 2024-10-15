56dbe240ba48cef79ba1918a3f95b7cf9dc2926c
allan_dg <amogusu@developmentgateway.org>
2024-10-15 11:35:06 +0300

    chore: Desktop/Tablet: Left configured legends appear on the Top Left side instead of Bottom Left side

--- ui/src/embeddable/chart/Pie.jsx
+++ ui/src/embeddable/chart/Pie.jsx
@@ -56,6 +56,8 @@ const Chart = ({
   const [wrapCount, setWrapCount] = useState(0);
   const [newMarginBottom, setNewMarginBottom] = useState(marginBottom);
 
+  console.log('lgend position...', legendPosition)
+
   const chartLegends = optionsVal.data
     .sort((a, b) => {
       if (a.position && b.position) {
@@ -95,8 +97,7 @@ const Chart = ({
 
   const leftLegendDynamicStyle = {
     bottom: `-${bottomSpacing}px`,
-    gap: "0px",
-    top: "0px",
+    gap: "0px"
   };
 
   const toggle = (id) => {
