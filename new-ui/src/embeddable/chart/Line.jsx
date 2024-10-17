13cf77bcf407ca80de741141f288e96d2aaf7e7c
Timothy Mugo <tmugo@developmentgateway.org>
2024-10-17 17:48:34 +0300

    DVIZ-10 add missing images and some code enhancements

--- new-ui/src/embeddable/chart/Line.jsx
+++ new-ui/src/embeddable/chart/Line.jsx
@@ -127,6 +127,7 @@ const Chart = ({
                   className={"ignore"}
                   type="checkbox"
                   checked={filter.length === 0 || !filter.includes(legend.id)}
+                  readOnly
                   style={{
                     backgroundColor:
                       legendCheckBack === true ? legend.color : "none",
