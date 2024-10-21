fd7e34e5eda8180475fa63b3ec0f7d03c729a96f
Timothy Mugo <tmugo@developmentgateway.org>
2024-10-21 19:22:12 +0300

    DVIZ-10 fix page modules,page gallery and vite env

--- new-ui/src/embeddable/pagemodules/index.tsx
+++ new-ui/src/embeddable/pagemodules/index.tsx
@@ -24,7 +24,7 @@ interface SectionHeaderProps {
 }
 
 export const SectionHeader: React.FC<SectionHeaderProps> = ({ title, subtitle, icon, media }) => {
-    return <Menu.Menu className="header title" text>
+    return <Menu className="header title" text>
         <Menu.Item>
             <Image src={media && media.guid ? media.guid.rendered : icon} />
         </Menu.Item>
@@ -34,7 +34,7 @@ export const SectionHeader: React.FC<SectionHeaderProps> = ({ title, subtitle, i
                 <h2 className="has-gray-color">{subtitle}</h2>
             </div>
         </Menu.Header>
-    </Menu.Menu>
+    </Menu>
 }
 
 const MediaImage = (props) => <img src={props.media && props.media.guid ? props.media.guid.rendered : null} />
