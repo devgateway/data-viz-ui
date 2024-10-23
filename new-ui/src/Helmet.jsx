05bf412129cd48d9f1ffcce4c41bc3136caa5516
Timothy Mugo <tmugo@developmentgateway.org>
2024-10-23 17:56:11 +0300

    DVIZ-10 migrate env variables to vite env variables

--- new-ui/src/Helmet.jsx
+++ new-ui/src/Helmet.jsx
@@ -4,11 +4,11 @@ import ReactHtmlParser from 'html-react-parser';
 
 console.log("env==>",process.env)
 
-const useHash = process.env.REACT_APP_USE_HASH_LINKS === true;
+const useHash = process.env.VITE_REACT_APP_USE_HASH_LINKS === true;
 
 export const replaceHTMLinks = (html, locale) => {
 
-    const replacementTarget = process.env.REACT_APP_WP_HOSTS.split(",");
+    const replacementTarget = process.env.VITE_REACT_APP_WP_HOSTS.split(",");
 
     const all = new RegExp("^(http|https)://(" + replacementTarget.join('|') + ")", "ig");
     let link;
