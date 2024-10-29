75d4ac630ca59cfadd1b790f21fdac740f9cc833
ralvarezigarzabal <ralvarezigarzabal@developmentgateway.org>
2024-10-29 10:16:48 -0300

    TCDICORE-132 comment out the function adjustDataSourceMargin from line 91 to 218

--- ui/src/embeddable/tabbedposts/index.jsx
+++ ui/src/embeddable/tabbedposts/index.jsx
@@ -88,7 +88,7 @@ const AccordionContent = ({ posts, activeItem, setActive }) => {
     }
   }, [scrollTarget]);
 
-  useEffect(() => {
+  /*useEffect(() => {
     let timeoutId;
     let observers = []; // Array to store observers for each accordion
 
@@ -215,7 +215,7 @@ const AccordionContent = ({ posts, activeItem, setActive }) => {
       clearTimeout(timeoutId);
       observers.forEach((observer) => observer.disconnect());
     };
-  }, [activeIndex, isMobileOrTablet]);
+  }, [activeIndex, isMobileOrTablet]);*/
 
   const handleClick = (e, titleProps) => {
     const { index } = titleProps;
