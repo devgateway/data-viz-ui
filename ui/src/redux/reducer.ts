d9606c3febcb5bd3fffa65ab7d6e8dc819aba466
Timothy Mugo <tmugo@developmentgateway.org>
2024-11-01 20:27:49 +0300

    DVIZ-10 add charts mobile config

--- ui/src/redux/reducer.ts
+++ ui/src/redux/reducer.ts
@@ -1,13 +1,18 @@
-import {combineReducers} from 'redux-immutable';
+import {combineReducers} from '@/lib/redux-immutable'
 import {wordpress} from "@devgateway/wp-react-lib"
 import {reducers} from '../embeddable'
 import { intlReducer } from '@/lib/react-intl-redux';
+import * as Immutable from 'immutable';
 
-
-const createRootReducer = () => combineReducers({
-    wordpress,
+const appReducers = {
     ...reducers,
+    wordpress,
     intl: intlReducer
-});
+}
+
+const initialState = () => Immutable.Map<string, any>();
+
+
+const createRootReducer = () => combineReducers(appReducers, initialState);
 
 export default createRootReducer
