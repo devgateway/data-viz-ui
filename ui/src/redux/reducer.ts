845913d3c8d0df4a2046b250c3cbc18bb6950a9a
Timothy Mugo <tmugo@developmentgateway.org>
2024-11-01 14:59:52 +0300

    DVIZ-10 remove unused deps and add react compiler

--- ui/src/redux/reducer.ts
+++ ui/src/redux/reducer.ts
@@ -1,25 +1,13 @@
 import {combineReducers} from 'redux-immutable';
 import {wordpress} from "@devgateway/wp-react-lib"
 import {reducers} from '../embeddable'
-import { createReduxHistoryContext } from "redux-first-history";
-import { createBrowserHistory } from "history";
 import { intlReducer } from '@/lib/react-intl-redux';
 
-const {
-    createReduxHistory,
-    routerMiddleware,
-    routerReducer
-} =
-    createReduxHistoryContext({
-        history: createBrowserHistory(),
-    })
 
 const createRootReducer = () => combineReducers({
-    router: routerReducer,
     wordpress,
     ...reducers,
     intl: intlReducer
 });
 
-export { createReduxHistory, routerMiddleware };
 export default createRootReducer
