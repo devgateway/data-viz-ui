7774e3ea8884bb4cb3c5b590e8f3ba854ea32f82
Timothy Mugo <tmugo@developmentgateway.org>
2024-11-01 20:46:34 +0300

    DVIZ-10 add maps mobile responsiveness

--- ui/src/embeddable/map/map.jsx
+++ ui/src/embeddable/map/map.jsx
@@ -14,7 +14,7 @@ import * as topojson from "topojson-client";
 import Legend from "./legend";
 import { formatContent } from "../common/MapTooltip";
 import getDeviceCategory from '../../utils/deviceType';
-import geoStats from 'geostats';
+import * as geoStats from 'geostats';
 
 const COLOR_VARIABLE = "_Color_";
 const LOCATION = "location";
@@ -24,28 +24,28 @@ const MAX_LABEL_LEN = 10;
 
 const breakpoints = {
   mobile: {
-      min: 320,
-      max: 480
+    min: 320,
+    max: 480
   },
   tablet: {
-      min: 481,
-      max: 768
+    min: 481,
+    max: 768
   },
   midTablet: {
-      min: 769,
-      max: 852,
+    min: 769,
+    max: 852,
   },
   laptop: {
-      min: 852,
-      max: 1024
+    min: 852,
+    max: 1024
   },
   desktop: {
-      min: 1025,
-      max: 1365
+    min: 1025,
+    max: 1365
   },
   wide: {
-      min: 1366,
-      max: Infinity
+    min: 1366,
+    max: Infinity
   }
 };
 
@@ -154,6 +154,7 @@ const colorSchemes = {
 };
 
 const isMobile = ['mobile', 'tablet', 'midTablet'].includes(getDeviceCategory());
+const isMobileOrTablet = ['mobile', 'tablet'].includes(getDeviceCategory());
 
 
 class Map extends React.Component {
@@ -211,8 +212,8 @@ class Map extends React.Component {
     this.state = {
       selectedMeasure:
         props.transformedData &&
-        props.transformedData.measures &&
-        props.transformedData.measures.length > 1
+          props.transformedData.measures &&
+          props.transformedData.measures.length > 1
           ? props.transformedData.measures[0]
           : null,
       generatedBreaks: [],
@@ -238,22 +239,22 @@ class Map extends React.Component {
   }
 
 
-handleScroll = () => {
-  // adds debounce to scroll to prevent event from rerendering the map too often
-  let scrollTimeout = null;
-  clearTimeout(scrollTimeout);
-  scrollTimeout = setTimeout(() => {
-    const labelsExist = d3.select(this.getMapId()).selectAll(".map-labels-container").size() > 0;
-    if (!labelsExist) {
-      this.updateFeatures(this.getFeatures(), false);
-    }
-  }, 300);
-}
+  handleScroll = () => {
+    // adds debounce to scroll to prevent event from rerendering the map too often
+    let scrollTimeout = null;
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(() => {
+      const labelsExist = d3.select(this.getMapId()).selectAll(".map-labels-container").size() > 0;
+      if (!labelsExist) {
+        this.updateFeatures(this.getFeatures(), false);
+      }
+    }, 300);
+  }
 
 
-componentWillUnmount() {
-  window.removeEventListener('scroll', this.handleScroll);
-}
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
 
   loadLayers() {
     const { source, mainLayerId, enabledLayers } = this.props;
@@ -382,7 +383,7 @@ componentWillUnmount() {
         mainLayer != prevState.mainLayer ||
         prevProps.mainLayerId !== this.props.mainLayerId ||
         JSON.stringify(prevProps.enabledLayers) !=
-          JSON.stringify(this.props.enabledLayers))
+        JSON.stringify(this.props.enabledLayers))
     ) {
       this.updateFeatures(this.getFeatures(), filterUpdated);
     }
@@ -442,31 +443,31 @@ componentWillUnmount() {
       .select("g")
       .selectAll(".map-labels-container");
 
-      labels.each((d, i, nodes) => {
-        const label = d3.select(nodes[i]);
-        const transform = d3.zoomTransform(label.node());
+    labels.each((d, i, nodes) => {
+      const label = d3.select(nodes[i]);
+      const transform = d3.zoomTransform(label.node());
 
-        const position = this.getLabelPosition(d);
-        let boxWidth = this.getLabelBoxWidth(d);
-        let boxHeight = this.getLabelBoxHeight(d);
+      const position = this.getLabelPosition(d);
+      let boxWidth = this.getLabelBoxWidth(d);
+      let boxHeight = this.getLabelBoxHeight(d);
 
-        if (d.properties[mapLabelField]) {
-          boxWidth = transform.k > 1 ? boxWidth / transform.k : boxWidth;
-          boxHeight = transform.k > 1 ? boxHeight / transform.k : boxHeight;
-        }
+      if (d.properties[mapLabelField]) {
+        boxWidth = transform.k > 1 ? boxWidth / transform.k : boxWidth;
+        boxHeight = transform.k > 1 ? boxHeight / transform.k : boxHeight;
+      }
 
-        const scalingFactor = Math.pow(transform.k, 0.5);
+      const scalingFactor = Math.pow(transform.k, 0.5);
 
-        label
-          .attr("x", position[0] - boxWidth / 2)
-          .attr("y", position[1] - (transform.k > 1 ? 10 / transform.k : 10))
-          .attr("width", boxWidth)
-          .attr("height", boxHeight)
-          .attr(
-            "font-size",
-            (transform.k > 1 ? labelFontSize / scalingFactor : labelFontSize) + "px"
-          );
-      });
+      label
+        .attr("x", position[0] - boxWidth / 2)
+        .attr("y", position[1] - (transform.k > 1 ? 10 / transform.k : 10))
+        .attr("width", boxWidth)
+        .attr("height", boxHeight)
+        .attr(
+          "font-size",
+          (transform.k > 1 ? labelFontSize / scalingFactor : labelFontSize) + "px"
+        );
+    });
   }
 
   resizePointLabels() {
@@ -541,8 +542,8 @@ componentWillUnmount() {
 
   classColor(d) {
     let { zoomEnabled } = this.props;
-    if(!zoomEnabled) {
-      zoomEnabled = ['mobile', 'tablet', 'midTablet'].includes(getDeviceCategory()) ? true: false;
+    if (!zoomEnabled) {
+      zoomEnabled = ['mobile', 'tablet', 'midTablet'].includes(getDeviceCategory()) ? true : false;
     }
     if (zoomEnabled) {
       return "active zoom-enabled";
@@ -1228,7 +1229,7 @@ componentWillUnmount() {
   showTooltip(event, d) {
     const {
       showTooltip,
-      
+
       tooltipTheme,
       customTooltips,
       tooltipFontSize,
@@ -1243,7 +1244,7 @@ componentWillUnmount() {
 
     let { zoomEnabled } = this.props;
 
-    zoomEnabled = ['mobile', 'tablet', 'midTablet'].includes(getDeviceCategory()) ? true: false;
+    zoomEnabled = ['mobile', 'tablet', 'midTablet'].includes(getDeviceCategory()) ? true : false;
 
     if (
       (showTooltip && d.properties.value != null) ||
@@ -1669,15 +1670,15 @@ componentWillUnmount() {
   d3Map(features, filterUpdated) {
     const { mapContainerBgColor, mapPosition, editing, mapType } =
       this.props;
-      let zoomEnabled = this.props.zoomEnabled;
-      if(!zoomEnabled) {
-        zoomEnabled = ['mobile', 'tablet'].includes(getDeviceCategory()) ? true: false;
-      }
+    let zoomEnabled = this.props.zoomEnabled;
+    if (!zoomEnabled) {
+      zoomEnabled = ['mobile', 'tablet'].includes(getDeviceCategory()) ? true : false;
+    }
     const breaks = this.getBreaks();
     const container = d3.select(this.getMapId());
     let svg = container.select("svg");
     let containerWidth = this.getWidth();
-    if(containerWidth === 0) {
+    if (containerWidth === 0) {
       containerWidth = window.innerWidth + deviceMapWidth[getDeviceCategory()];
     } else {
       containerWidth += deviceMapWidth[getDeviceCategory()];
@@ -1739,17 +1740,17 @@ componentWillUnmount() {
             .translate(mapPosition.x, mapPosition.y)
             .scale(mapPosition.k),
         );
-        if(mapType === 'POINTS_MAP') {
-          const deviceTranslates = {
-            'mobile': 100,
-            'tablet': 0,
-            'midTablet': 0,
-            'desktop': 0,
-            'laptop': 0,
-            'wide': 0
-          }
-          const translateVal = deviceTranslates[getDeviceCategory()];
-          svg
+      if (mapType === 'POINTS_MAP') {
+        const deviceTranslates = {
+          'mobile': 100,
+          'tablet': 0,
+          'midTablet': 0,
+          'desktop': 0,
+          'laptop': 0,
+          'wide': 0
+        }
+        const translateVal = deviceTranslates[getDeviceCategory()];
+        svg
           .transition()
           .duration(300)
           .call(
@@ -1758,7 +1759,7 @@ componentWillUnmount() {
               .translate(mapPosition.x + translateVal, mapPosition.y)
               .scale(mapPosition.k),
           );
-        }
+      }
     }
 
     if (zoomEnabled || editing) {
@@ -1894,8 +1895,8 @@ componentWillUnmount() {
       zoomEnabled
     } = this.props;
 
-    if(!zoomEnabled) {
-      zoomEnabled = ['mobile', 'tablet', 'midTablet'].includes(getDeviceCategory()) ? true: false;
+    if (!zoomEnabled) {
+      zoomEnabled = ['mobile', 'tablet', 'midTablet'].includes(getDeviceCategory()) ? true : false;
     }
     const nationalAverage = this.getAvg();
     const filters = this.getFilters();
@@ -1910,82 +1911,87 @@ componentWillUnmount() {
       highlightedLocStyle.marginTop = "25px";
     }
 
+    const MapLegendComponent = () => (
+      <Container fluid className={"footnote "}>
+        {
+          <Grid columns={2}>
+            {app !== "csv" && showOverallValue && (
+              <Grid.Column textAlign={"left"} width={4}>
+                <div className="national-average-div">
+                  <span className="national-avg-label">
+                    {nationalAverageLabel}
+                  </span>
+                  <span className="national-avg-value">
+                    {formatContent(
+                      valueFormat,
+                      { value: nationalAverage },
+                      intl,
+                      noDataText,
+                    )}
+                  </span>
+                </div>
+              </Grid.Column>
+            )}
+            <Grid.Column
+              textAlign={"right"}
+              width={app !== "csv" && showOverallValue ? 12 : 16}
+            >
+              <Legend
+                filteredBreaks={this.getBreaks()}
+                formattedLegendTitle={formatContent(
+                  legendTitle,
+                  { ...filters },
+                  intl,
+                  noDataText,
+                )}
+                selectedMeasure={this.state.selectedMeasure}
+                {...this.props}
+              />
+            </Grid.Column>
+          </Grid>
+        }
+        <div className="measure-selector">
+          <ul>
+            {measureSelectorLabel && (
+              <li>
+                <span className="label">{measureSelectorLabel}</span>
+              </li>
+            )}
+            {transformedData &&
+              transformedData.measures &&
+              transformedData.measures.length > 1 &&
+              transformedData.measures.map((measure) => {
+                return (
+                  <li
+                    onClick={this.selectedMeasureChanged.bind(
+                      this,
+                      measure,
+                    )}
+                  >
+                    <input
+                      checked={this.getSelectedMeasure() === measure}
+                      type="radio"
+                      readOnly
+                      value={measure}
+                    />
+                    <label>
+                      {transformedData.measureLabelMap[measure] ||
+                        measure}
+                    </label>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+      </Container>
+    )
+
     return (
       <div className="map component wp-data-viz-map" ref={this.mapContainer}>
         {this.state.layersLoading && this.renderLoader()}
         {!this.state.layersLoading && (
           <>
-            <Container fluid className={"footnote "}>
-              {
-                <Grid columns={2}>
-                  {app !== "csv" && showOverallValue && (
-                    <Grid.Column textAlign={"left"} width={4}>
-                      <div className="national-average-div">
-                        <span className="national-avg-label">
-                          {nationalAverageLabel}
-                        </span>
-                        <span className="national-avg-value">
-                          {formatContent(
-                            valueFormat,
-                            { value: nationalAverage },
-                            intl,
-                            noDataText,
-                          )}
-                        </span>
-                      </div>
-                    </Grid.Column>
-                  )}
-                  <Grid.Column
-                    textAlign={"right"}
-                    width={app !== "csv" && showOverallValue ? 12 : 16}
-                  >
-                    <Legend
-                      filteredBreaks={this.getBreaks()}
-                      formattedLegendTitle={formatContent(
-                        legendTitle,
-                        { ...filters },
-                        intl,
-                        noDataText,
-                      )}
-                      selectedMeasure={this.state.selectedMeasure}
-                      {...this.props}
-                    />
-                  </Grid.Column>
-                </Grid>
-              }
-              <div className="measure-selector">
-                <ul>
-                  {measureSelectorLabel && (
-                    <li>
-                      <span className="label">{measureSelectorLabel}</span>
-                    </li>
-                  )}
-                  {transformedData &&
-                    transformedData.measures &&
-                    transformedData.measures.length > 1 &&
-                    transformedData.measures.map((measure) => {
-                      return (
-                        <li
-                          onClick={this.selectedMeasureChanged.bind(
-                            this,
-                            measure,
-                          )}
-                        >
-                          <input
-                            checked={this.getSelectedMeasure() === measure}
-                            type="radio"
-                            value={measure}
-                          />
-                          <label>
-                            {transformedData.measureLabelMap[measure] ||
-                              measure}
-                          </label>
-                        </li>
-                      );
-                    })}
-                </ul>
-              </div>
-            </Container>
+            {!isMobileOrTablet && <MapLegendComponent />}
             <div
               className={"map wrapper scaling-svg-container " + unique}
               style={{ height: this.props.height - deviceMapHeight[getDeviceCategory()] + "px" }}
@@ -2035,6 +2041,7 @@ componentWillUnmount() {
                 </div>
               )}
             </div>
+            {isMobileOrTablet && <MapLegendComponent />}
           </>
         )}
       </div>
