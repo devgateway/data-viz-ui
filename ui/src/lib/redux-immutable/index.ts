211a3325678034f4f23714470b35cb93f562e8db
Timothy Mugo <tmugo@developmentgateway.org>
2024-11-01 20:14:08 +0300

    DVIZ-10 add redux-immutable library

---
+++ ui/src/lib/redux-immutable/index.ts
@@ -0,0 +1,51 @@
+import { Action, ReducersMapObject } from "redux";
+import Immutable from "immutable";
+import { getUnexpectedInvocationParameterMessage } from "./utils";
+
+export const combineReducers = <S extends Immutable.Map<string, any>>(
+  reducers: ReducersMapObject<any, any>,
+  getDefaultState: () => S
+): ((inputState: S | undefined, action: Action) => S) => {
+  const reducerKeys = Object.keys(reducers);
+
+  // eslint-disable-next-line space-infix-ops
+  return (inputState: S | undefined, action: Action): S => {
+    if (typeof inputState === "undefined") {
+      inputState = getDefaultState();
+    }
+
+    // eslint-disable-next-line no-process-env
+    if (process.env.NODE_ENV !== "production") {
+      const warningMessage = getUnexpectedInvocationParameterMessage(
+        inputState,
+        reducers,
+        action
+      );
+
+      if (warningMessage) {
+        // eslint-disable-next-line no-console
+        console.error(warningMessage);
+      }
+    }
+
+    return inputState.withMutations(temporaryState => {
+      reducerKeys.forEach(reducerName => {
+        const reducer = reducers[reducerName];
+        const currentDomainState = temporaryState.get(reducerName);
+        const nextDomainState = reducer(currentDomainState, action);
+
+        if (nextDomainState === undefined) {
+          throw new Error(
+            'Reducer "' +
+              reducerName +
+              '" returned undefined when handling "' +
+              action.type +
+              '" action. To ignore an action, you must explicitly return the previous state.'
+          );
+        }
+
+        temporaryState.set(reducerName, nextDomainState);
+      });
+    });
+  };
+};
