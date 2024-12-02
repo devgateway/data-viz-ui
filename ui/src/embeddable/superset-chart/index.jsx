6c6d180e7bd8c8872f5eae3575550c1dc865ac73
gmutuhu <geraldmutuhu@gmail.com>
2024-12-02 08:24:21 +0300

    ALDASH-52 fix preview issue on WP
    ALDASH-52 fix preview issue on WP

--- ui/src/embeddable/superset-chart/index.jsx
+++ ui/src/embeddable/superset-chart/index.jsx
@@ -9,25 +9,22 @@ const Component = (props) => {
         "data-csv": csv = "",
         "data-selected-chart-data": selectedChartData = "{}",
         "data-apache-superset-url": apacheSupersetUrl = "",
-        "data-height": height
-      
+        "data-height": height      
     } = props
 
-    const url = decodeURI(apacheSupersetUrl);    
+    const url = decodeURIComponent(apacheSupersetUrl);    
     const iframeSrc = `${url}/superset/explore/?form_data=${selectedChartData}&standalone=1`;   
     return (
-        <div id={`superset-chart-${unique}`} style={{"height": height + 'px'}} >
+        <div id={`superset-chart-${unique}`} style={{"height": height + 'px'}} >           
             <iframe
                 src={iframeSrc}
                 width="100%"   
                 height={height + 'px'}           
                 seamless="seamless"
                 title="superset-chart"
-            />            
+            />          
         </div>);    
 }
-
-
 
 export default Component;
 
