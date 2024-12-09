c5399da193d6686ac21d55ed950e39ea75d6ffb8
Timothy Mugo <tmugo@developmentgateway.org>
2024-12-09 19:59:11 +0300

    chore:fix gitignore

--- ui/build/client/assets/selectAll-xUNDRJmB.js
+++
@@ -1,1 +0,0 @@
-import{S as t,s as n,o}from"./transform-jezyhA6t.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([n(e)],o)}export{a as s};
