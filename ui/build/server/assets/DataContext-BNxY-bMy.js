954b585417e5c19f36a459efc5958ca362bc836a
Timothy Mugo <tmugo@developmentgateway.org>
2024-12-09 19:59:11 +0300

    chore:fix gitignore

--- ui/build/server/assets/DataContext-BNxY-bMy.js
+++
@@ -1,7 +0,0 @@
-import React__default from "react";
-const DataContext = React__default.createContext();
-const CategoriesContext = React__default.createContext();
-export {
-  CategoriesContext as C,
-  DataContext as D
-};
