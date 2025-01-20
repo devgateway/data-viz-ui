a55c91accdc3bda9622c8ce9557be7fe5e6bf725
gmutuhu <geraldmutuhu@gmail.com>
2025-01-20 10:40:48 +0300

    ALDASH-77 configure superset url in WP
    ALDASH-77 configure superset url in WP

--- custom/wp-customizer/blocks/index.jsx
+++ custom/wp-customizer/blocks/index.jsx
@@ -2,5 +2,3 @@ require('./wrapper/index.js')
 
 require('./example/index.js')
 require('./json-data-provider/index.js')
-require('./superset-chart/index.js')
-require('./superset-dashboard/index.js')
