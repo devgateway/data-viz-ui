b769880d7bac98b1705b78419930742799a0a7dd
gmutuhu <geraldmutuhu@gmail.com>
2025-02-21 10:54:45 +0300

    ALDASH-112 get superset url from settings
    ALDASH-112 get superset url from settings

--- ui/src/embeddable/superset-chart/index.jsx
+++ ui/src/embeddable/superset-chart/index.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import {connect} from "react-redux";
+import { SettingProvider } from '@devgateway/wp-react-lib';
+import {SettingsConsumer} from '@devgateway/wp-react-lib';
 
-const Component = (props) => {
+const SupersetChartWrapper = (props) => {
+  return (<SettingProvider locale={props.intl.locale} changeUUID={props.unique}>
+            <SettingsConsumer>
+                <SupersetChart {...props} />
+            </SettingsConsumer>
+            </SettingProvider>);
+}
+        
+const SupersetChart = (props) => {
     const {
         editing = false,
         unique,
         intl,        
         "data-csv": csv = "",
         "data-selected-chart-data": selectedChartData = "{}",
-        "data-apache-superset-url": apacheSupersetUrl = "",
         "data-height": height,  
         "data-width": width = 100,   
-        "data-margin": margin = 0
+        "data-margin": margin = 0,
+         settings
     } = props;
 
+    const apacheSupersetUrl = settings ? settings.apache_superset_url: "";
     const url = decodeURIComponent(apacheSupersetUrl);  
     const chartdata = decodeURIComponent(selectedChartData);  
     const iframeSrc = `${url}/superset/explore/?form_data=${chartdata}&standalone=1`;   
@@ -35,5 +46,5 @@ const mapStateToProps = (state, ownProps) => {
 const mapActionCreators = {};
 
 
-export default connect(mapStateToProps, mapActionCreators)(Component);
+export default connect(mapStateToProps, mapActionCreators)(SupersetChartWrapper);
 
