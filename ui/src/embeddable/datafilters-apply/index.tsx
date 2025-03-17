f45b2df30be4a59f19c042b3f91e94f7b0eb5ebc
Sebastian Dimunzio <sdimunzio@gmail.com>
2025-03-17 14:34:33 -0300

    ALDASH-75 adding apply button

--- ui/src/embeddable/datafilters-apply/index.tsx
+++ ui/src/embeddable/datafilters-apply/index.tsx
@@ -1,34 +1,50 @@
-import React from 'react';
-import { connect } from "react-redux";
-import { Container, Icon } from "semantic-ui-react";
-import { cleanFilter } from "../reducers/data";
+import React, {useEffect, useRef, useState} from 'react';
+import {connect} from "react-redux";
+import {Button, Container, Icon} from "semantic-ui-react";
+import {applyFilter} from "../reducers/data";
 
 
 const FiltersResetComponent = (props) => {
     const {
+        editing = false,
         appliedFilters,
-        initialFilters,
         "data-group": group,
-        onClean,
+        onApply,
         "data-app": app = "csv",
-        "data-reset-label": resetLabel = "Reset All Filters"
+        "data-label": label = "Apply"
     } = props
 
-    let enabled = false;
 
-    //TODO: Check why we are using Number.MIN_SAFE_INTEGER instead of an empty array
-    Object.keys(initialFilters).forEach(k => {
-        if (initialFilters[k].length != appliedFilters[k].filter(v => v != Number.MIN_SAFE_INTEGER).length) {
-            enabled = true
+    function usePrevious(value) {
+        const ref = useRef();
+        useEffect(() => {
+            ref.current = value;
+        });
+        return ref.current;
+    }
+
+    const [enabled, setEnabled] = useState(false)
+    const prevFilters = usePrevious(appliedFilters);
+
+    useEffect(() => {
+
+        debugger; //eslint-disable-line
+        if (prevFilters) {
+            Object.keys(prevFilters).forEach(k => {
+                if (prevFilters[k].length != appliedFilters[k].filter(v => v != Number.MIN_SAFE_INTEGER).length) {
+                    setEnabled(true)
+                }
+            });
         }
-    })
-
+    }, [appliedFilters])
 
 
     return (
-        <Container fluid={true} className={`data-filters-reset ignore ${enabled ? '' : "disabled"}`} onClick={e => onClean({ app, group })}>
-            <span>{resetLabel}</span>
-            <span><Icon name="undo alternate" className="custom-undo-icon" /></span>
+        <Container fluid={true}
+                   className={`data-filters-apply ignore ${enabled ? '' : "disabled"} ${editing ? 'editing' : ''}`}
+                   onClick={e => onApply({app, group})}>
+            <span><Icon name="chevron circle right" className="custom-apply-icon"/></span>
+            <span>{label}</span>
         </Container>
     );
 };
@@ -47,7 +63,7 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 const mapActionCreators = {
-    onClean: cleanFilter
+    onApply: applyFilter
 };
 
 export default connect(mapStateToProps, mapActionCreators)(FiltersResetComponent)
