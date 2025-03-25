d4161dd9b42dc6c073ff39352023e6488d0e2ae0
Sebastian Dimunzio <sdimunzio@gmail.com>
2025-03-25 11:41:00 -0300

    updating default api URL

--- ui/src/embeddable/reducers/data-api.js
+++ ui/src/embeddable/reducers/data-api.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-debugger */
 import { Config } from '@/conf'
 import {get} from '../../api/commons'
-const API_ROOT = process.env.VITE_REACT_APP_API_ROOT ? process.env.VITE_REACT_APP_API_ROOT : "http://localhost"; //"http://localhost"; //Config.REACT_APP_API_ROOT;
-
+const API_ROOT = process.env.REACT_APP_API_ROOT
 console.log("API_ROOT==>", API_ROOT);
 
 function queryParams(params) {
