c95202462c86cb282dcce0649fa0ae02941a3937
gmutuhu <geraldmutuhu@gmail.com>
2025-04-01 09:54:07 +0300

    ALDASH-157 fix sorting issues
    ALDASH-157 fix sorting issues

--- ui/src/embeddable/chart/data/Utils.js
+++ ui/src/embeddable/chart/data/Utils.js
@@ -40,4 +40,26 @@ export const getTranslatedValue = (obj, locale) => {
   return null;
 };
 
-export default { measuresMap, typesMap, measureLabel, getTranslatedValue };
+export const alphaSort = (reverse, locale, a, b) => {
+  return new Intl.Collator(locale, {
+      caseFirst: "upper",
+      numeric: true,
+      sensitivity: "variant",
+  }).compare(reverse ? b : a, reverse ? a : b);
+};
+
+export const numericSort = (reverse, a, b) => {
+  return reverse ? b - a : a - b;
+};
+
+export const dateSort = (reverse, a, b) => {
+  let aDate = Date.parse(a + "");
+  let bDate = Date.parse(b + "");
+ 
+  if (!isNaN(aDate) && !isNaN(bDate)) {
+      return reverse ? bDate - aDate : aDate - bDate;
+  }
+
+  return 0;
+}
+export default { measuresMap, typesMap, measureLabel, getTranslatedValue, alphaSort, numericSort, dateSort };
