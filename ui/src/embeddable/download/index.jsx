1e33c2b1355b4b9de2440691ff69e59adf03d50c
allan_dg <amogusu@developmentgateway.org>
2025-04-25 16:53:49 +0300

    fix(TCDICORE-258): fix page module component showing mobile styles
    - fixes issue with page module component showing mobile styles for
      components in edit mode
    - makes react components responsive within the page module component
      inside wordpress, so that when switching between preview modes, the
    view of the component is displayed.
    - fixes related issue with mobile and tablet rotations

--- ui/src/embeddable/download/index.jsx
+++ ui/src/embeddable/download/index.jsx
@@ -3,197 +3,198 @@ import { Button, Container, Dropdown, Grid, Icon } from "semantic-ui-react";
 import { PostContent } from "@devgateway/wp-react-lib";
 import { domtoimage } from "./dom-to-image";
 import { saveAs } from 'file-saver';
-
+import { connect } from 'react-redux';
 
 const DownloadableContent = React.forwardRef((props, ref) => (
-    <div ref={ref}>{props.children}</div>
+  <div ref={ref}>{props.children}</div>
 ));
 
-
 const DownloadComponent = (props) => {
-    const componentRef = useRef();
-    const {
-        childContent,
-        "data-height": height,
-        "data-button-label": buttonLabel,
-        "data-png-label": pngLabel,
-        "data-jpg-label": jpgLabel,
-        'data-jpg-text': jpgText,
-        'data-png-text': pngText,
-        "data-check-png": checkPNG = 'true',
-        "data-check-jpg": checkJPG = 'true',
-        "data-title": title,
-        "data-default-format": defaultFormat = "PNG",
-        "data-use-title": useTitle = "false",
-        "data-style": style = "heavy",
-        "data-section-title": sectionTitle = "",
-        "data-download-tooltip": tooltip = "",
-        "data-include-source-url": includeSourceURL = "false",
-        "data-source-urlmargin-left": sourceURLMarginLeft = 70,
-        "data-source-urlmargin-top": sourceURLMarginTop = 10,
-        "data-source-urlfont-size": sourceURLFontSize = 18,
-        parent,
-        editing,
-        component,
-        unique
+  const componentRef = useRef();
+  let {
+    childContent,
+    "data-height": height,
+    "data-button-label": buttonLabel,
+    "data-png-label": pngLabel,
+    "data-jpg-label": jpgLabel,
+    'data-jpg-text': jpgText,
+    'data-png-text': pngText,
+    "data-check-png": checkPNG = 'true',
+    "data-check-jpg": checkJPG = 'true',
+    "data-title": title,
+    "data-default-format": defaultFormat = "PNG",
+    "data-use-title": useTitle = "false",
+    "data-style": style = "heavy",
+    "data-section-title": sectionTitle = "",
+    "data-download-tooltip": tooltip = "",
+    "data-include-source-url": includeSourceURL = "false",
+    "data-source-urlmargin-left": sourceURLMarginLeft = 70,
+    "data-source-urlmargin-top": sourceURLMarginTop = 10,
+    "data-source-urlfont-size": sourceURLFontSize = 18,
+    parent,
+    editing,
+    component,
+    unique,
+    pageModuleProps
+  } = props;
 
-    } = props
+  const [fileType, setFileType] = useState(defaultFormat);
+  const isCheckPNG = checkPNG == 'true' || checkPNG === true;
+  const isCheckJPG = checkJPG == 'true' || checkJPG === true;
 
+  useEffect(() => {
+    setFileType(defaultFormat);
+  }, [defaultFormat]);
 
-    const [fileType, setFileType] = useState(defaultFormat)
-    const isCheckPNG = checkPNG == 'true' || checkPNG == true
-    const isCheckJPG = checkJPG == 'true' || checkJPG == true
+  const handleChange = (e) => {
+    setFileType(e.target.value);
+  };
 
-
-    useEffect(() => {
-        setFileType(defaultFormat)
-    }, [defaultFormat])
-
-    const handleChange = (e) => {
-        setFileType(e.target.value)
+  function filter(node) {
+    const attributes = node.attributes;
+    const attributeNames = [];
+    if (attributes) {
+      for (let i = 0; i < attributes.length; i++) {
+        attributeNames.push(attributes[i].nodeName);
+      }
     }
 
-    function filter(node) {
-        const attributes = node.attributes;
-        const attributeNames = []
-        if (attributes) {
-            for (let i = 0; i < attributes.length; i++) {
-                attributeNames.push(attributes[i].nodeName);
-            }
-        }
-
-        const customAttributes = attributeNames.filter(a => a.startsWith('data-'));
-        if (customAttributes.length > 0) {
-            customAttributes.forEach(name => {
-                node.setAttribute(name, "")
-            })
-        }
-
-        if (node.classList) {
-            return !node.classList.contains("ignore")
-        }
-
-        return true;
+    const customAttributes = attributeNames.filter(a => a.startsWith('data-'));
+    if (customAttributes.length > 0) {
+      customAttributes.forEach(name => {
+        node.setAttribute(name, "")
+      });
     }
 
-    const options = {filter, bgcolor: "#FFF"}
-    const save = (type) => {
-
-         domtoimage.cloneNode(componentRef.current).then(function (node) {
-
-             [...node.getElementsByTagName("input")].forEach(e=>e.remove());
-
-             // Remove all elements that are icons
-             [...node.querySelectorAll(".question.circle.icon")].forEach(e => e.remove());
-
-              //add source url
-              const addSourceURL = includeSourceURL == "true";
-              if (addSourceURL) {
-                  const urlNode = document.createElement('div')
-                  urlNode.style.marginLeft = sourceURLMarginLeft + "px"
-                  urlNode.style.marginTop = sourceURLMarginTop + "px"
-                  urlNode.style.fontSize = sourceURLFontSize + "px"
-                  urlNode.style.fontFamily = 'Roboto, sans-serif';
-                  urlNode.style.fontWeight = '400';
-                  urlNode.style.color = '#66676d';
-                  urlNode.style.opacity = '0.75';
-                  urlNode.innerHTML = window.location.href
-                  urlNode.style.maxWidth = "90%"; // Set a max width for the container
-                  urlNode.style.wordWrap = "break-word"; // Break lines within words if necessary
-                  urlNode.style.overflowWrap = "break-word"; // Ensure compatibility with other browsers
-                  node.appendChild(urlNode)
-              }
-
-             options.height = componentRef.current.scrollHeight + 90;
-             options.width = componentRef.current.scrollWidth + 30;
-              node.style.padding = "20px"
-
-              if (type == "PNG") {
-                  domtoimage.toPng(node, options)
-                      .then(function (blob) {
-                          saveAs(blob, pngLabel)
-                      });
-              }
-
-              if (type == "JPG") {
-                  domtoimage.toJpeg(node, options)
-                      .then(function (blob) {
-                          saveAs(blob, jpgLabel)
-                      });
-              }
-          })
+    if (node.classList) {
+      return !node.classList.contains("ignore");
     }
 
-    const onClickHandler = (type) => {
-        if (editing) {
-            alert("Not allowed when editing please preview page")
-        } else {
-            save(type)
+    return true;
+  }
+
+  const options = { filter, bgcolor: "#FFF" };
+
+  const save = (type) => {
+    domtoimage.cloneNode(componentRef.current).then(function (node) {
+      [...node.getElementsByTagName("input")].forEach(e => e.remove());
+      [...node.querySelectorAll(".question.circle.icon")].forEach(e => e.remove());
+
+      const addSourceURL = includeSourceURL === "true";
+      if (addSourceURL) {
+        const urlNode = document.createElement('div');
+        urlNode.style.marginLeft = sourceURLMarginLeft + "px";
+        urlNode.style.marginTop = sourceURLMarginTop + "px";
+        urlNode.style.fontSize = sourceURLFontSize + "px";
+        urlNode.style.fontFamily = 'Roboto, sans-serif';
+        urlNode.style.fontWeight = '400';
+        urlNode.style.color = '#66676d';
+        urlNode.style.opacity = '0.75';
+        urlNode.innerHTML = window.location.href;
+        urlNode.style.maxWidth = "90%";
+        urlNode.style.wordWrap = "break-word";
+        urlNode.style.overflowWrap = "break-word";
+        node.appendChild(urlNode);
+      }
+
+      options.height = componentRef.current.scrollHeight + 90;
+      options.width = componentRef.current.scrollWidth + 30;
+      node.style.padding = "20px";
+
+      if (type === "PNG") {
+        domtoimage.toPng(node, options)
+          .then(function (blob) {
+            saveAs(blob, pngLabel);
+          });
+      }
+
+      if (type === "JPG") {
+        domtoimage.toJpeg(node, options)
+          .then(function (blob) {
+            saveAs(blob, jpgLabel);
+          });
+      }
+    });
+  };
+
+  const onClickHandler = (type) => {
+    if (editing) {
+      alert("Not allowed when editing please preview page");
+    } else {
+      save(type);
+    }
+  };
+
+  return (
+    <Container
+      className={`viz download ${style} ${useTitle ? 'has-title' : ''}
+        ${(isCheckPNG || isCheckJPG) ? 'has-formats' : ''}
+        ${(editing || pageModuleProps?.editing) ? 'editing' : ''}
+        ${(isCheckPNG && isCheckJPG) ? 'multi-format' : ''}
+        ${(isCheckPNG && !isCheckJPG) || (!isCheckPNG && isCheckJPG) ? 'single-format' : ''}`}
+      fluid={true}
+    >
+      <DownloadableContent ref={componentRef}>
+        <Grid stackable reversed={"mobile"} className={"download-header"}>
+          {!editing && useTitle === "true" &&
+            <Grid.Column width={12}>
+              <PostContent parentUnique={props.unique}
+                post={{ content: { rendered: decodeURIComponent(sectionTitle) } }} />
+            </Grid.Column>}
+          <Grid.Column className={editing ? "editing ignore" : "ignore"} width={(editing || useTitle !== "true") ? 16 : 4}
+            textAlign={"right"}>
+            <div className={"wrapper"}>
+              <Button className={"download"} onClick={() => onClickHandler(fileType)}>
+                {buttonLabel} {fileType === 'PNG' ? 'PNG' : 'JPG'}
+              </Button>
+              <Dropdown className={"download"} data-tooltip={decodeURIComponent(tooltip)}
+                trigger={(isCheckJPG && isCheckPNG) ?
+                  <Icon name={"download"} className='download-icon'></Icon> : null}>
+                <Dropdown.Menu>
+                  {title}
+                  {(isCheckPNG === true) && (
+                    <Dropdown.Item onClick={() => {
+                      setFileType('PNG');
+                      onClickHandler('PNG');
+                    }}>
+                      <input type='radio' value='PNG' checked={fileType === 'PNG'} onChange={handleChange} />
+                      <label>{pngText}</label>
+                    </Dropdown.Item>
+                  )}
+                  {(isCheckJPG === true) && (
+                    <Dropdown.Item onClick={() => {
+                      setFileType('JPG');
+                      onClickHandler('JPG');
+                    }}>
+                      <input type='radio' value='JPG' checked={fileType === 'JPG'} onChange={handleChange} />
+                      <label>{jpgText}</label>
+                    </Dropdown.Item>
+                  )}
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          </Grid.Column>
+        </Grid>
+        {!editing &&
+          <Container fluid={true} className={`download area ${editing || pageModuleProps?.editing ? 'editing' : ''}`}>
+            <PostContent parentUnique={props.unique}
+              post={{ content: { rendered: childContent } }} />
+          </Container>
         }
-    }
-
-    return (
-
-        <Container
-            className={`viz download ${style} ${useTitle ? 'has-title' : ''}
-                ${(isCheckPNG || isCheckJPG) ? 'has-formats' : ''}
-                ${editing ? 'editing' : ''}
-                ${(isCheckPNG && isCheckJPG) ? 'multi-format' : ''}
-                ${(isCheckPNG && !isCheckJPG) || (!isCheckPNG && isCheckJPG) ? 'single-format' : ''}`}
-            fluid={true}>
-
-            <DownloadableContent ref={componentRef}>
-                <Grid stackable reversed={"mobile"} className={"download-header"}>
-                    {!editing && useTitle == "true" &&
-                        <Grid.Column width={12}>
-                            <PostContent parentUnique={props.unique}
-                                         post={{content: {rendered: decodeURIComponent(sectionTitle)}}}></PostContent>
-
-                        </Grid.Column>}
-                    <Grid.Column className={ editing ? "editing ignore" : "ignore"  } width={(editing || useTitle != "true") ? 16 : 4}
-                                 textAlign={"right"}>
-                        <div className={"wrapper"}>
-                            <Button className={"download"} onClick={() => onClickHandler(fileType)}>
-                                {buttonLabel} {fileType === 'PNG' ? 'PNG' : 'JPG'}
-                            </Button>
-                            <Dropdown className={"download"} data-tooltip={decodeURIComponent(tooltip)}
-                                      trigger={(isCheckJPG && isCheckPNG) ?
-                                          <Icon name={"download"} className='download-icon'></Icon> : null}>
-                                <Dropdown.Menu>
-                                    {title}
-                                    {(isCheckPNG == 'true' || isCheckPNG == true) ? (
-                                        <Dropdown.Item onClick={() => {
-                                            setFileType('PNG');
-                                            onClickHandler('PNG');
-                                        }}>
-                                            <input type='radio' value='PNG' checked={fileType === 'PNG'} onChange={handleChange} />
-                                            <label>{pngText}</label>
-                                        </Dropdown.Item>
-                                    ) : null}
-                                    {(isCheckJPG == 'true' || isCheckJPG == true) ? (
-                                        <Dropdown.Item onClick={() => {
-                                            setFileType('JPG');
-                                            onClickHandler('JPG');
-                                        }}>
-                                            <input type='radio' value='JPG' checked={fileType === 'JPG'} onChange={handleChange} />
-                                            <label>{jpgText}</label>
-                                        </Dropdown.Item>
-                                    ) : null}
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </div>
-                    </Grid.Column>
-                </Grid>
-                {!editing &&
-                    <Container fluid={true} className={"download area"}>
-                        <PostContent parentUnique={props.unique}
-                                     post={{content: {rendered: childContent}}}></PostContent>
-                    </Container>
-                }
-            </DownloadableContent>
-        </Container>
-
-    );
+      </DownloadableContent>
+    </Container>
+  );
 };
 
-export default DownloadComponent;
+const mapStateToProps = (state) => {
+  const pageModuleProps = state.getIn(["data", "pageModuleProps"]);
+  const _props = {};
+  if (pageModuleProps) {
+    _props.pageModuleProps = pageModuleProps;
+  }
+  return _props;
+};
+
+const mapActionCreators = {};
+
+export default connect(mapStateToProps, mapActionCreators)(DownloadComponent);
