1e7e8a4ce91b729442235c7dfaeb25f5692a7577
Timothy Mugo <tmugo@developmentgateway.org>
2025-05-20 12:45:14 +0300

    chore(DVIZ-43): update dependencies and enhance example app configuration. Fix some issues in the dvz ui

---
+++ example/app/routes/previewType.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { PreviewTypeContainer } from "@devgateway/dvz-ui-react/layout";
+
+const PreviewTypeRoute = ({ params }: any) => {
+  // const { lan, type, id } = params;
+  return <PreviewTypeContainer />;
+};
+
+export default PreviewTypeRoute;
