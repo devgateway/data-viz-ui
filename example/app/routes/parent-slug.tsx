8457b3d992c9974a1376a48d5449af5956fc2d77
Timothy Mugo <tmugo@developmentgateway.org>
2025-05-21 14:05:25 +0300

    chore(DVIZ-43): implement locale redirection in root loader and refactor routing structure.

--- example/app/routes/parent-slug.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { SlugContainer } from "@devgateway/dvz-ui-react/layout";
-
-const SlugRoute = ({ params }: any) => {
-  // const { lan, slug, parent } = params;
-  return <SlugContainer />;
-};
-
-export default SlugRoute;
