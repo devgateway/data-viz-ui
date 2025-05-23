404bc23ddddf8d0a168856c01463f6ee353209be
Timothy Mugo <tmugo@developmentgateway.org>
2025-05-23 07:37:12 +0300

    chore (DVIZ-43): implement SEO optimization for routes and remove unused assets

--- example/app/routes/slugPost.tsx
+++ example/app/routes/slugPost.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { SlugPostContainer } from "@devgateway/dvz-ui-react/layout";
 
 const SlugPostRoute = ({ params }: any) => {
-  // const { lan, year, month, day, slug, parent } = params;
   return <SlugPostContainer />;
 };
 
