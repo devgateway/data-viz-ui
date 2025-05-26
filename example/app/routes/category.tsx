1824a54a02750e35c8e2239a1a50cef2d9a5b521
Timothy Mugo <tmugo@developmentgateway.org>
2025-05-26 15:15:18 +0300

    feat(DVIZ-43): implement default locale constant and enhance routing with loading fallbacks

--- example/app/routes/category.tsx
+++ example/app/routes/category.tsx
@@ -1,7 +1,13 @@
 import React, { lazy } from "react";
 import { Category } from "@devgateway/wp-react-lib";
+import Loading from "~/components/layout/Loading";
 
 const ResponsiveContainer = lazy(() => import("@devgateway/dvz-ui-react/layout"));
+
+export function HydrateFallback() {
+  return <Loading />
+}
+
 
 const CategoryRoute = ({ params }: any) => {
   const { lan, slug } = params;
