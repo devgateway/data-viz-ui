01775f99cd4363eafddf71247ee220d4787e5736
Timothy Mugo <tmugo@developmentgateway.org>
2025-05-26 15:29:56 +0300

    refactor(DVIZ-43): enable custom header and footer

--- packages/dvz-ui/src/layout/ResponsiveContainer.tsx
+++ packages/dvz-ui/src/layout/ResponsiveContainer.tsx
@@ -1,21 +1,27 @@
 import React from 'react'
 
 import {Container,} from 'semantic-ui-react'
-import PropTypes from 'prop-types'
 import {SettingsConsumer} from '@devgateway/wp-react-lib'
 import {Media} from "@/utils/AppMedia"
 import Footer from "./Footer.jsx";
 import Header from "./Header.jsx";
 import TopNavigator from "./TopNavigator.jsx";
-import { CustomizerWrapper } from "./Customizer";
+import { CustomizerWrapper } from "./Customizer.jsx";
 
 
-const DesktopContainer = ({ children, fixed }) => {
+export interface DesktopContainerProps {
+    children: React.ReactNode;
+    fixed: boolean;
+    header?: React.ReactNode;
+}
+
+
+const DesktopContainer = ({ children, fixed = false, header }: DesktopContainerProps) => {
     return (
         <Container fluid={true}>
             <SettingsConsumer>
                 <CustomizerWrapper>
-                    <Header />
+                   { header ? header : <Header />}
                 </CustomizerWrapper>
             </SettingsConsumer>
             <Container className="desktop">
@@ -26,24 +32,31 @@ const DesktopContainer = ({ children, fixed }) => {
     )
 }
 
-DesktopContainer.propTypes = {
-    children: PropTypes.node,
+
+export interface ResponsiveContainerProps {
+    children: React.ReactNode;
+    fixed?: boolean;
+    locale?: string;
+    pages?: any[];
+    header?: React.ReactNode;
+    footer?: React.ReactNode;
 }
 
 
-function ResponsiveContainer (props) {
+function ResponsiveContainer (props: ResponsiveContainerProps) {
 
-    const {children, fixed, locale, pages} = props
+    const {children, fixed, locale, pages, header, footer} = props;
     const page = pages ? pages[0] : null;
 
         return (<div>
             <style>
+                {/* @ts-ignore */}
                 {Media.mediaStyles}
             </style>
-            <DesktopContainer fixed={fixed}>
+            <DesktopContainer fixed={fixed!} header={header}>
                 {children}
             </DesktopContainer>
-            {page && page.template === "noofoter.php" ? "" : <Footer />}
+            {footer ? footer : (page && page.template === "noofoter.php" ? "" : <Footer />)}
         </div>)
 }
 
