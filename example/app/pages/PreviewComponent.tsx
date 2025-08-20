65b7825055b0603a8fe55de54261d65eef87dd92
Sebastian Dimunzio <sdimunzio@gmail.com>
2025-08-20 08:30:16 -0300

    Transforming params into a plain object, this will fix the preview on edit mode

--- example/app/pages/PreviewComponent.tsx
+++ example/app/pages/PreviewComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useMemo } from 'react';
 import { useLocation, useParams, useSearchParams } from 'react-router';
 import { getComponentByNameIgnoreCase } from '@devgateway/dvz-ui-react';
 import { Container, Segment } from 'semantic-ui-react';
@@ -12,6 +12,9 @@ const PreviewComponentParameterParser = () => {
     const [UIComponent] = useState(() => getComponentByNameIgnoreCase(urlParams.name ?? ''));
 
     let [params, setParams] = useSearchParams();
+
+    //eslint-disable-next-line
+    debugger;
     const readMessage = (event: MessageEvent) => {
         console.log("-------------------------------reading message ----------------------------------------")
         const data = event.data
@@ -38,11 +41,15 @@ const PreviewComponentParameterParser = () => {
             window.removeEventListener('message', readMessage);
         };
     }, []);
+
+
+    const paramProps = React.useMemo(() => Object.fromEntries(params.entries()), [params]);
+
     return (
         <div>
             <Container fluid={true} className={"editing"}>
                 {/* @ts-ignore */}
-                {UIComponent ? <UIComponent  {...params} editing={true} /> :
+                {UIComponent ? <UIComponent  {...paramProps} editing={true} /> :
                     <Segment.Group color={"red"} textAlign={"center"}><h1>Wrong Component Name</h1></Segment.Group>}
             </Container>
 
