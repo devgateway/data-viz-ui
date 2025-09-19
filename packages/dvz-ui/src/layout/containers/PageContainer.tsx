0e35f235c58f82650c6ed24e951c47aca2934109
Sebastian Dimunzio <sdimunzio@gmail.com>
2025-09-19 12:18:12 -0300

    adding new containers for post and pages.

---
+++ packages/dvz-ui/src/layout/containers/PageContainer.tsx
@@ -0,0 +1,29 @@
+import {
+    PageProvider,
+    PageConsumer,
+    Page
+} from "@devgateway/wp-react-lib"
+import React from 'react'
+import {useParams} from 'react-router';
+import ResponsiveContainer from '../ResponsiveContainer';
+
+interface SlugContainerProps {
+    header?: React.ReactNode;
+    footer?: React.ReactNode;
+    page: Object;
+}
+
+const PageContainer = ({header, footer, page}: SlugContainerProps) => {
+    const {lan: locale, slug} = useParams();
+
+    //eslint-disable-next-line
+    debugger;
+    return (
+
+        <ResponsiveContainer header={header} footer={footer}>
+            <Post></Post>
+        </ResponsiveContainer>
+    )
+}
+
+export default PageContainer
