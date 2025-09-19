42d3ba0c8bac3e73c26468a3b5e6f24eb6fc5b88
Sebastian Dimunzio <sdimunzio@gmail.com>
2025-09-19 12:16:09 -0300

    updating dev version

--- packages/dvz-ui/src/embeddable/big-number/index.jsx
+++ packages/dvz-ui/src/embeddable/big-number/index.jsx
@@ -83,7 +83,7 @@ const Chart = (props) => {
 
     const dimensions = []   
     return (<div ref={ref}>
-        <h1>TEST</h1>
+        <h1>TEST 1</h1>
          <Container className={"chart container big-number-container"} style={{"height": height + 'px'}} fluid={true}>
              <DataProvider
                 style={{"height": `${contentHeight}px`}}
