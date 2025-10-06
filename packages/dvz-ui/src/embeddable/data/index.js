4eb47d3b992e51cd39c763e49b8e6b24d5eaca25
Sebastian Dimunzio <sdimunzio@gmail.com>
2025-10-06 11:23:42 -0300

    adding additional exports

---
+++ packages/dvz-ui/src/embeddable/data/index.js
@@ -0,0 +1,6 @@
+export * from './CategoriesConsumer.jsx'
+export * from './CategoriesProvider.jsx'
+export * from './DataConsumer.jsx'
+export * from './DataConsumerMap.jsx'
+export * from './DataContext.jsx'
+export * from './DataProvider.jsx'
