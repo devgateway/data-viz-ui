6fd41139f34361267aa8d7ddbedffc406120b550
Sebastian Dimunzio <sdimunzio@gmail.com>
2025-10-08 16:53:03 -0300

    adding additional exports

--- packages/dvz-ui/src/index.ts
+++ packages/dvz-ui/src/index.ts
@@ -7,6 +7,9 @@ export {
 
 export {default as DataConsumer} from "./embeddable/data/DataConsumer"
 export {default as DataProvider} from "./embeddable/data/DataProvider"
+
+export {decode, parse, compareJsonProps} from "./embeddable/utils/parseUtils"
+export *  as reducer from "./embeddable/reducers/data"
 // export * from "./conf";
 // export * from "./layout";
 export * from "./tracker";
