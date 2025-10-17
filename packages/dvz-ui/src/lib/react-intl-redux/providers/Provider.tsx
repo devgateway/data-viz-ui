533f4ed052f174c7495d62bc93b3a011ce79023f
Timothy Mugo <tmugo@developmentgateway.org>
2025-10-17 15:48:50 +0300

    fix: (TCDICORE-340): fix env variables improt

--- packages/dvz-ui/src/lib/react-intl-redux/providers/Provider.tsx
+++ packages/dvz-ui/src/lib/react-intl-redux/providers/Provider.tsx
@@ -4,9 +4,12 @@ import PropTypes from 'prop-types'
 
 import IntlProvider from './IntlProvider'
 
+// @ts-ignore Types not available
+const DEFAULT_LOCALE = import.meta.env.VITE_REACT_APP_DEFAULT_LOCALE ?? process.env.VITE_REACT_APP_DEFAULT_LOCALE ?? "en";
+
 const Provider = ({ store, children }) =>
   <ReduxProvider store={store}>
-    <IntlProvider locale={process.env.VITE_REACT_APP_DEFAULT_LOCALE as string}>
+    <IntlProvider locale={DEFAULT_LOCALE as string}>
       {children}
     </IntlProvider>
   </ReduxProvider>
