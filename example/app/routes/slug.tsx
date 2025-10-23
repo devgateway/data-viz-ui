0ce4dcbdd99c736ee3f4c5c3390626089d4b7b05
Timothy Mugo <tmugo@developmentgateway.org>
2025-10-23 11:00:15 +0300

    chore (TCDICORE-340): Use dynamic API url from the environment at production. The URL can also be retrieved from the origin/referrer. - Improve SSR context

--- example/app/routes/slug.tsx
+++ example/app/routes/slug.tsx
@@ -4,11 +4,13 @@ import { getPages } from "@devgateway/wp-react-lib";
 import { getMetaSeo } from "~/utils/meta-seo";
 import { SlugContainer } from '@devgateway/dvz-ui-react/layout'
 import Header from "~/embeddable/Header";
+import { getApiUrl } from "~/utils/api-utils";
 
 export async function loader({ request, params}: Route.LoaderArgs) {
   const posts = await getPages({
     slug: params.slug ?? "home",
     locale: params.lan,
+    apiBaseUrl: getApiUrl(request),
   });
 
   const findPost = posts.data.find(post => post.slug === params.slug);
