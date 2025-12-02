37793c1b8d7b9f3c02b4d5662445de077ae64da4
gmutuhu <geraldmutuhu@gmail.com>
2025-12-02 15:48:58 +0300

    ALDASH-497 bars component improvements
    ALDASH-497 bars component improvements

--- packages/dvz-ui/src/embeddable/grouped-bars/GroupedBars.jsx
+++ packages/dvz-ui/src/embeddable/grouped-bars/GroupedBars.jsx
@@ -4,6 +4,7 @@ import DataProvider from "../data/DataProvider.jsx";
 import DataConsumer from "../data/DataConsumer.jsx";
 import { connect } from "react-redux";
 import { alphaSort } from "../utils/common.js";
+import { formatContent } from "../common/ChartTooltip.jsx";
 
 const DEFAULT_NO_DATA_MESSAGE = "No data matches your selection";
 const DEFAULT_NO_DATA_TEXT = "-";
@@ -88,6 +89,8 @@ const BarItem = ({
     labelPosition,
     valuePosition,
     labelWidth,
+    labelFormat,
+    vars,
     intl 
 }) => {    
     
@@ -103,9 +106,11 @@ const BarItem = ({
         display: 'flex',              
         alignItems: 'center',    
         height: '32px'           
-    };
+    }; 
 
-    if (labelPosition === 'left') {
+    let lformat = decodeURIComponent(labelFormat);
+    let labelString = formatContent(lformat, vars ? vars : {value: dimensionValue}, intl);
+    if (labelPosition === 'left') {      
         return (
             <div className="grouped-bar-item" style={{ marginBottom: "10px" }}>
                 <div style={{ 
@@ -113,8 +118,7 @@ const BarItem = ({
                     alignItems: "flex-start",    // allow taller label without shrinking bar
                     gap: "12px"
                 }}>
-                    <div className="grouped-bar-label" style={labelStyle}>
-                        {dimensionValue}
+                    <div className="grouped-bar-label" style={labelStyle} dangerouslySetInnerHTML={{__html: labelString}}>
                     </div>
                     <div 
                         className="grouped-bar-bar-container" 
@@ -166,8 +170,7 @@ const BarItem = ({
                 gap: "12px",
                 marginBottom: "6px"
             }}>
-                <div className="grouped-bar-label" style={labelStyle}>
-                    {dimensionValue}
+                <div className="grouped-bar-label" style={labelStyle} dangerouslySetInnerHTML={{__html: labelString}}>
                 </div>
                 {valuePosition === 'top' && (
                     <div 
@@ -252,7 +255,8 @@ const DataFrame = (props) => {
         barBackgroundColor,
         labelPosition,
         valuePosition,
-        labelWidth
+        labelWidth,
+        labelFormat
     } = props;
 
     
@@ -279,7 +283,8 @@ const DataFrame = (props) => {
             const dataItems = children.map(d => ({
                 value: d.value,
                 [measureField]: d[measureField],
-                [dimensionField]: d.value
+                [dimensionField]: d.value,
+                vars: {...d}
             }));
 
             return { dataItems, measureField, dimensionField };
@@ -331,6 +336,8 @@ const DataFrame = (props) => {
                         labelPosition={labelPosition}
                         valuePosition={valuePosition}
                         labelWidth={labelWidth}
+                        labelFormat={labelFormat}
+                        vars={item.vars}
                     />
                 );
             })}
@@ -365,10 +372,10 @@ const Chart = (props) => {
         "data-label-position": labelPosition,
         "data-value-position": valuePosition,
         "data-label-width": labelWidth,
+        "data-label-format": labelFormat
     } = props;
 
     
-
     const ref = useRef(null);
     const [mode, setMode] = useState(editMode);
     
@@ -420,7 +427,9 @@ const Chart = (props) => {
                             barBackgroundColor={barBackgroundColor}
                             labelPosition={labelPosition}
                             valuePosition={valuePosition}
-                            labelWidth={labelWidth}/>
+                            labelWidth={labelWidth}
+                            labelFormat={labelFormat}
+                            />
                     </DataConsumer>
                 </DataProvider>
             </Container>
