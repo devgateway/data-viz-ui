811bbddd12b832f25a2cf65d9c3b87b5b1016dba
Timothy Mugo <tmugo@developmentgateway.org>
2026-01-13 17:39:14 +0300

    fix (TCDICORE-431): React Markdown to render CSS in production

--- packages/dvz-ui/src/embeddable/chart/Tooltip.jsx
+++ packages/dvz-ui/src/embeddable/chart/Tooltip.jsx
@@ -9,6 +9,12 @@ const numericExpresion = /(\+?\#)[\(]([A-z0-9,.,-]+)\)/gi;
 const compactExpresion = /(\+?\#C)[\(]([A-z0-9,.,-]+)\)/gi;
 
 const applyFormat = (expresion, str, style, isPercent, intl, container) => {
+  // If intl is not available (e.g., during SSR), use a simple fallback formatter
+  if (!intl || !intl.formatNumber) {
+    // Return string as-is if intl is not available
+    return str;
+  }
+
   let result;
   let str1 = str;
   while ((result = expresion.exec(str)) !== null) {
@@ -33,6 +39,11 @@ export const formatContent = (
   intl,
   tooltipEnableMarkdown
 ) => {
+  // Guard against undefined/null values
+  if (!tooltip || !variables) {
+    return "";
+  }
+
   // if variables have a property called "field" and another property with the value being _${field},
   // add _value to the variables object with the value of the _${field} property
   if (variables.field && variables[`_${variables.field}`]) {
@@ -58,13 +69,19 @@ export const formatContent = (
 };
 
 const Tooltip = ({ tooltip, d, intl, tooltipEnableMarkdown }) => {
-  const { color, data } = d.datum || d.point || d;
+  // Guard against undefined/null values during SSR
+  if (!d || !tooltip) {
+    return <div></div>;
+  }
+
+  const datum = d.datum || d.point || d;
+  const { color, data } = datum || {};
   const current =
     d.value ||
     (d.datum ? d.datum.value : null) ||
     (d.point ? d.point.data.y : null);
-  if (data) {
 
+  if (data) {
     let vars;
     if (data.variables) {
       vars = typeof data.variables[d.id] === 'object'
@@ -75,16 +92,22 @@ const Tooltip = ({ tooltip, d, intl, tooltipEnableMarkdown }) => {
     }
 
     const params = {
-      field: d.point ? d.point.serieId : d.id,
+      field: d.point ? d.point.serieId : (d.id || ''),
       ...vars,
       value: current,
     };
 
-    if (data.measureFieldName) {
+    if (data.measureFieldName && data.variables) {
       params.populationValue =
         data.variables[data.measureFieldName + "Population"];
     }
+
     const str = formatContent(tooltip, params, intl, tooltipEnableMarkdown);
+
+    if (!str) {
+      return <div></div>;
+    }
+
     if (tooltipEnableMarkdown) {
       return (
         <div className={"chart tooltip"}>
@@ -94,7 +117,6 @@ const Tooltip = ({ tooltip, d, intl, tooltipEnableMarkdown }) => {
             rehypePlugins={[rehypeRaw]}
           ></ReactMarkdown>
         </div>
-
       );
     } else {
       return (
